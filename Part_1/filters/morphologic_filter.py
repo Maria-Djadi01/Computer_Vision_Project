f0366f3ebececa96817ed00c3e77cfb3f568b41d
imikaa <151672286+imikaa@users.noreply.github.com>
2023-12-09 11:51:26 +0100

    Update morphologic_filter.py

--- Part_1/filters/morphologic_filter.py
+++ Part_1/filters/morphologic_filter.py
@@ -3,9 +3,26 @@ import cv2
 import numpy as np
 
 # ------------------ Dilation
-def dilation( img, kernel ) :
+def dilation( img, kernel_size, kernel_shape, iterations=1 ) :
+    if kernel_shape == 'rect' :
+        kernel_list = [ [1 for i in range(kernel_size)] for j in range(kernel_size) ]
+        kernel = np.array( kernel_list )
+    elif kernel_shape == 'cross' :
+        kernel_list = [
+            [1 if i == kernel_size // 2 or j == kernel_size // 2 else 0 for i in range(kernel_size)]
+            for j in range(kernel_size)
+        ]
+        kernel = np.array( kernel_list )
+    elif kernel_shape == 'ellipse' :
+        kernel = np.zeros((kernel_size, kernel_size), dtype=np.uint8)
+        center = (kernel_size - 1) / 2
+        radius = center
+        for i in range(kernel_size):
+            for j in range(kernel_size):
+                if ((i - center) / radius) ** 2 + ((j - center) / radius) ** 2 <= 1:
+                    kernel[i, j] = 1
+                    
     height, width = img.shape
-    k_height, k_width = kernel.shape
     img_result = np.zeros( (height, width) )
 
     i = 0
@@ -17,14 +34,14 @@ def dilation( img, kernel ) :
             if img[i, j] == 255 :
                 
                 h = 0
-                while h < k_height :
+                while h < kernel_size :
                     
                     w = 0
-                    while w < k_width :
+                    while w < kernel_size :
                         
                         if kernel[h, w] == 1 :
-                            row = i - k_height // 2 + h
-                            col = j - k_width // 2 + w
+                            row = i - kernel_size // 2 + h
+                            col = j - kernel_size // 2 + w
                             if(height>row>=0 and width>col>=0 ) :
                                 img_result[row, col] = 255
                             
@@ -39,9 +56,26 @@ def dilation( img, kernel ) :
     return img_result
 
 # ------------------ Erosion
-def erosion( img, kernel ) :
+def erosion( img, kernel_size, kernel_shape, iterations=1 ) :
+    if kernel_shape == 'rect' :
+        kernel_list = [ [1 for i in range(kernel_size)] for j in range(kernel_size) ]
+        kernel = np.array( kernel_list )
+    elif kernel_shape == 'cross' :
+        kernel_list = [
+            [1 if i == kernel_size // 2 or j == kernel_size // 2 else 0 for i in range(kernel_size)]
+            for j in range(kernel_size)
+        ]
+        kernel = np.array( kernel_list )
+    elif kernel_shape == 'ellipse' :
+        kernel = np.zeros((kernel_size, kernel_size), dtype=np.uint8)
+        center = (kernel_size - 1) / 2
+        radius = center
+        for i in range(kernel_size):
+            for j in range(kernel_size):
+                if ((i - center) / radius) ** 2 + ((j - center) / radius) ** 2 <= 1:
+                    kernel[i, j] = 1
+                    
     height, width = img.shape
-    k_height, k_width = kernel.shape
     img_result = np.zeros( (height, width) )
 
     i = 0
@@ -54,13 +88,13 @@ def erosion( img, kernel ) :
                 
                 all_pixels_are_one = True
                 h = 0
-                while h < k_height :
+                while h < kernel_size :
                     
                     w = 0
-                    while w < k_width :
+                    while w < kernel_size :
                         if kernel[h, w] == 1 :
-                            row = i - k_height // 2 + h
-                            col = j - k_width // 2 + w
+                            row = i - kernel_size // 2 + h
+                            col = j - kernel_size // 2 + w
                             if(height>row>=0 and width>col>=0) :
                                 if img[row, col] != 255 :
                                     all_pixels_are_one = False
@@ -83,16 +117,16 @@ def erosion( img, kernel ) :
     return img_result
 
 # ------------------ Opening
-def opening( img, kernel ) :
-    return dilation( erosion( img, kernel ), kernel )
+def opening( img, kernel_size, kernel_shape, iterations=1 ) :
+    return dilation( erosion( img, kernel_size, kernel_shape, iterations=1 ), kernel_size, kernel_shape, iterations=1 )
     
 # ------------------ Closing
-def closing( img, kernel ) :
-    return erosion( dilation( img, kernel ), kernel )
+def closing( img, kernel_size, kernel_shape, iterations=1 ) :
+    return erosion( dilation( img, kernel_size, kernel_shape, iterations=1 ), kernel_size, kernel_shape, iterations=1 )
 
 
 
-# ------------------ Test
+# ------------------ Test examples
 """
 img = cv2.imread( "j.png", 0 )
 img1 = cv2.imread( "j1.png", 0 )
@@ -112,10 +146,10 @@ kernel = np.array([[0, 0, 0, 0, 1, 0, 0, 0, 0],
        [0, 0, 0, 0, 1, 0, 0, 0, 0],
        [0, 0, 0, 0, 1, 0, 0, 0, 0]])
 
-img_result_dilation = dilation( img, kernel )
-img_result_erosion = erosion( img, kernel )
-img_result_opening = opening( img1, kernel )
-img_result_closing = closing( img2, kernel )
+img_result_dilation = dilation( img, 9, 'cross' )
+img_result_erosion = erosion( img, 9, 'cross' )
+img_result_opening = opening( img1, 9, 'cross' )
+img_result_closing = closing( img2, 9, 'cross' )
 
 cv2.imshow( "image originale", img )
 cv2.imshow( "image originale 1", img1 )
@@ -125,18 +159,4 @@ cv2.imshow( "image erosion", img_result_erosion )
 cv2.imshow( "image opening", img_result_opening )
 cv2.imshow( "image closing", img_result_closing )
 cv2.waitKey(0)
-cv2.destroyAllWindows()
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
+cv2.destroyAllWindows()"""
