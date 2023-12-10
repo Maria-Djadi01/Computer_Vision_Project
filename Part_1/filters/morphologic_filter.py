912202384530eee2860298bd5d9fb828d4d11160
imikaa <151672286+imikaa@users.noreply.github.com>
2023-12-10 19:07:52 +0100

    Update morphologic_filter.py

--- Part_1/filters/morphologic_filter.py
+++ Part_1/filters/morphologic_filter.py
@@ -9,7 +9,6 @@ parameters :
     iterations : optional, by default 1.
 """
 
-
 # ------------------ Dilation
 def dilation(img, kernel_size, kernel_shape, iterations):
     if kernel_shape == "rect":
@@ -37,28 +36,20 @@ def dilation(img, kernel_size, kernel_shape, iterations):
     img_result = np.zeros((height, width))
 
     for iteration in range(iterations):
-        i = 0
-        while i < height:
-            j = 0
-            while j < width:
-                if img[i, j] == 255:
-                    h = 0
-                    while h < kernel_size:
-                        w = 0
-                        while w < kernel_size:
+        
+        for i in range( height ) :
+            for j in range( width ) :
+                
+                if img[i, j] > 0:
+            
+                    for h in range( kernel_size ) :
+                        for w in range( kernel_size ) :
+                            
                             if kernel[h, w] == 1:
                                 row = i - kernel_size // 2 + h
                                 col = j - kernel_size // 2 + w
                                 if height > row >= 0 and width > col >= 0:
-                                    img_result[row, col] = 255
-
-                            w += 1
-
-                        h += 1
-
-                j += 1
-
-            i += 1
+                                    img_result[row, col] = img[i,j]
 
         img = img_result.copy()
 
@@ -92,39 +83,32 @@ def erosion(img, kernel_size, kernel_shape, iterations):
     img_result = np.zeros((height, width))
 
     for iteration in range(iterations):
-        i = 0
-        while i < height :
-            
-            j = 0
-            while j < width:
-                if img[i, j] == 255:
+        
+        for i in range( height ) :
+            for j in range( width ) :
+                
+                if img[i, j] > 0:
                     all_pixels_are_one = True
-                    h = 0
-                    while h < kernel_size:
-                        w = 0
-                        while w < kernel_size:
+                    
+                    for h in range( kernel_size ) :
+                        for w in range( kernel_size ) :
+                            
                             if kernel[h, w] == 1:
                                 row = i - kernel_size // 2 + h
                                 col = j - kernel_size // 2 + w
                                 if height > row >= 0 and width > col >= 0:
-                                    if img[row, col] != 255:
+                                    if img[row, col] == 0:
                                         all_pixels_are_one = False
                                         break
-
-                            w += 1
 
                         if not all_pixels_are_one:
                             break
 
-                        h += 1
-
                     if all_pixels_are_one:
-                        img_result[i, j] = 255
-
-                j += 1
-
-            i += 1
+                        img_result[i, j] = img[i, j]
+                        
         img = img_result.copy()
+        
     return img_result
 
 
@@ -148,37 +132,8 @@ def closing(img, kernel_size, kernel_shape, iterations):
     )
 
 
-# ------------------ Test examples
-"""
-img = cv2.imread( "j.png", 0 )
-img1 = cv2.imread( "j1.png", 0 )
-img2 = cv2.imread( "j2.png", 0 )
 
-cv2.threshold( img, 130, 255, 0, img )
-cv2.threshold( img1, 130, 255, 0, img1 )
-cv2.threshold( img2, 130, 255, 0, img2 )
 
-kernel = np.array([[0, 0, 0, 0, 1, 0, 0, 0, 0],
-       [0, 0, 0, 0, 1, 0, 0, 0, 0],
-       [0, 0, 0, 0, 1, 0, 0, 0, 0],
-       [0, 0, 0, 0, 1, 0, 0, 0, 0],
-       [1, 1, 1, 1, 1, 1, 1, 1, 1],
-       [0, 0, 0, 0, 1, 0, 0, 0, 0],
-       [0, 0, 0, 0, 1, 0, 0, 0, 0],
-       [0, 0, 0, 0, 1, 0, 0, 0, 0],
-       [0, 0, 0, 0, 1, 0, 0, 0, 0]])
 
-img_result_dilation = dilation( img, 9, 'cross', iterations=2 )
-img_result_erosion = erosion( img, 9, 'cross', iterations=2 )
-img_result_opening = opening( img1, 9, 'cross', iterations=2 )
-img_result_closing = closing( img2, 9, 'cross', iterations=2 )
 
-cv2.imshow( "image originale", img )
-cv2.imshow( "image originale 1", img1 )
-cv2.imshow( "image originale 2", img2 )
-cv2.imshow( "image dilation", img_result_dilation )
-cv2.imshow( "image erosion", img_result_erosion )
-cv2.imshow( "image opening", img_result_opening )
-cv2.imshow( "image closing", img_result_closing )
-cv2.waitKey(0)
-cv2.destroyAllWindows()"""
+
